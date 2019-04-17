ba1031256351fc7b6f2a74f2781f4a0952f65b0a
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-04-17 11:23:26 +0300

    Finished for review

--- assingment.playground/Contents.swift
+++ assingment.playground/Contents.swift
@@ -2,73 +2,41 @@ import Foundation
 
 private let workplace = Workplace()
 
-workplace.setEmptySeat(count: 7)
-workplace.employeesInOffice(employeeCount: 5)
-workplace.seats
+workplace.setEmptySeat(count: 9)
+workplace.employeesInOffice(employeeCount: 6)
 
-public func isSeatAvailable(seatIndex: Int) -> Bool {
-    // if index of seat [] != nil
-    
-    var seatAvailabilityCheck = false
-    for seat in workplace.seats {
-        if seatIndex >= 0 && seatIndex <= workplace.seats.count {
-            if seat == nil {
-            print("the seat number \(seatIndex) is free")
-            seatAvailabilityCheck = true
-            break
-        } else if seat != nil {
-            print("the seat number \(seatIndex) is occupied")
-            seatAvailabilityCheck = false
-            break
-        }
-        } else {
-            print ("oh no! something went wrong! seat index must be between 0 and seat number count!")
-            break
-        }
+public func isSeatAvailable(_ seatIndex: Int) -> Bool {
+    let seat = workplace.seats[seatIndex]
+    if seat == nil {
+        return true
+    } else {
+        return false
     }
-    return seatAvailabilityCheck
 }
 
 public func occupyEmptySeat(index: Int, employee: String = "👩🏻‍💼") {
-    
-    var checkIfSeatIsAvailable = isSeatAvailable(seatIndex: index)
-    for seat in workplace.seats {
-        if index >= 0 && index <= workplace.seats.count {
-            if checkIfSeatIsAvailable == true {
-                print ("\(employee) got new workplace number \(index)")
-//                seat?.append(employee)
-                break
-            } else {
-                print("\(employee) tried to take seat number \(index), unfortunately that seat was occupied by \(seat)")
-                break
-            }
+    if index >= 0 && index < workplace.seats.count {
+    if isSeatAvailable(index) {
+        workplace.seats[index] = employee
+    } else {
+        if let oldEmployee = workplace.seats[index] {
+            print("\(employee) tried to take seat number \(index + 1), unfortunatelly that seat was occupied by \(oldEmployee).")
         }
     }
- // find and set up value to employee
-    // if seat == nil {employees new place = index}
-    // var oldemployee
-    //else {
-    //print("\(employee) tried to take seat number \(index), unfortunatelly that seat was occupied by \(oldEmployee).")
+    } else {
+            print ("Something went wrong! index must be between 0 and \(workplace.seats.count - 1)!")
+    }
 }
 
 public func workIsLeftBy(employee: String) {
-    //iterate through currently present workers
-    // if employee == nil {"employee seems to be out-of-office."}
-    for seat in workplace.seats {
+    for index in 0 ... workplace.seats.count - 1 {
+        let seat = workplace.seats[index]
         if seat == employee {
-            print ("\(seat) should leave workplace")
-//            seat?.remove(at: String.Index)
-            break
-        } else {
-            print ("\(employee) seems to be out-of-office.")
-            break
+            workplace.seats[index] = nil
         }
     }
+        print("\(employee) seems to be out-of-office.")
 }
-workplace.seats
-isSeatAvailable(seatIndex: 0)
-occupyEmptySeat(index: 0, employee: "👩🏻‍💼")
+
+occupyEmptySeat(index: 8, employee: "👩🏻‍💼")
 workIsLeftBy(employee: "👩🏼‍💻")
-
-
-
