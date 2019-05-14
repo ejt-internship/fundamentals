01fc134289395b3922e86f4d419225731a07ce3a
Edgars Vanags <e.vanags@accenture.com>
2019-05-15 01:36:06 +0300

    Unit tests: Added test target and ViewControllerTests with boilerplate

---
+++ FundamentalsAssignment/FundamentalsAssignmentTests/ViewControllerTests.swift
@@ -0,0 +1,16 @@
+//
+//  ViewControllerTests.swift
+//  ViewControllerTests
+//
+//  Created by e.vanags on 14/05/2019.
+//  Copyright © 2019 egmars.janis.timma. All rights reserved.
+//
+
+import XCTest
+import SFundamentals
+
+@testable import FundamentalsAssignment
+
+class ViewControllerTests: XCTestCase {
+
+}
