f0e1379013d1d1f3d4a5a48267b786b127d646d7
Egmars Janis Timma <egmars.janis.timma@accenture.com>
2019-05-15 17:52:47 +0300

    Deleted unnecessary classes

--- FundamentalsAssignment/FundamentalsAssignment/CarCell.swift
+++
@@ -1,9 +0,0 @@
-//
-//  CarCell.swift
-//  FundamentalsAssignment
-//
-//  Created by egmars.janis.timma on 02/05/2019.
-//  Copyright © 2019 egmars.janis.timma. All rights reserved.
-//
-
-import Foundation
